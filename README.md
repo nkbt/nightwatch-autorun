6a2b48440afb2055a54b61e9fd51dc7c59778202
Nik Butenko <nik@butenko.me>
2015-11-07 22:30:39 +1100

    Implement nightwatch autorun

--- README.md
+++ README.md
@@ -7,11 +7,11 @@ Automatically installs Selenium (if necessary) and runs End-to-End tests with Ni
 
 1. Installs Selenium
 
-2. Starts Selenium on default 4444 port with `-debug` outputs logs to `$LOG_DIR` or `./reports/selenium.log`
+2. Starts Selenium on default 4444 port with `-debug` outputs logs to `$LOG_DIR` or `$(pwd)/reports/selenium.log`
 
-3. Starts WebpackDevServer based on `webpack.config.js` on port `$PORT` or 8080
+3. Starts WebpackDevServer based on `$WEBPACK_CONFIG` or `$(pwd)/webpack.config.js` on port `$PORT` or 8080
 
-4. Runs Nightwatch with `nightwatch.json` config and outputs reports to `$REPORT_DIR` ot `./reports/test-e2e`
+4. Runs Nightwatch with `$NIGHTWATCH_CONFIG` or `$(pwd)/nightwatch.json` or baked in `nightwatch.json` config and outputs reports to `$REPORT_DIR` or `$(pwd)/reports/test-e2e`
 
 
 ## Requirements
---
+++ index.js
@@ -0,0 +1,98 @@
+#!/usr/bin/env node
+
+var selenium = require('selenium-standalone');
+var cp = require('child_process');
+var path = require('path');
+var fs = require('fs');
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var webpackConfig = require(process.env.WEBPACK_CONFIG ?
+  path.resolve(process.env.WEBPACK_CONFIG) : path.resolve(process.cwd(), 'webpack.config.js'));
+var logDir = process.env.LOG_DIR ?
+  path.resolve(process.env.LOG_DIR) : path.resolve(process.cwd(), 'reports');
+var reportDir = process.env.REPORT_DIR ?
+  path.resolve(process.env.REPORT_DIR) : path.resolve(process.cwd(), 'reports', 'test-e2e');
+var nightwatchConfig = process.env.NIGHTWATCH_CONFIG ?
+  path.resolve(process.env.NIGHTWATCH_CONFIG) : path.resolve(process.cwd(), 'nightwatch.json');
+
+
+try {
+  require(nightwatchConfig);
+} catch (err) {
+  console.error(err);
+  console.log(err.stack);
+  console.log('Fallback to default nightwatch.json config');
+  nightwatchConfig = path.resolve(__dirname, 'nightwatch.json');
+}
+
+
+var seleniumLog = fs.createWriteStream(path.resolve(logDir, 'selenium.log'));
+var nightwatchRunner = path
+  .resolve(process.cwd(), 'node_modules', '.bin', 'nightwatch');
+
+
+function startServer(cb) {
+  var server = new WebpackDevServer(webpack(webpackConfig), {quiet: true});
+
+  server.listen(process.env.PORT || 8080, '0.0.0.0', cb);
+}
+
+
+function onServerStarted(seleniumChild) {
+  return function (err) {
+    if (err) {
+      console.error(err);
+      console.log(err.stack);
+      throw err;
+    }
+
+    cp.fork(nightwatchRunner,
+      [
+        '--config', nightwatchConfig,
+        '--output', reportDir
+      ])
+      .on('error', function (err2) {
+        console.error(err2);
+        console.log(err2.stack);
+        throw err2;
+      })
+      .on('close', function (code) {
+        seleniumChild.kill('SIGINT');
+        process.exit(code);
+      });
+  };
+}
+
+
+function onSeleniumStarted(err, seleniumChild) {
+  if (err) {
+    console.error(err);
+    console.log(err.stack);
+    throw err;
+  }
+  seleniumChild.stdout.pipe(seleniumLog);
+  seleniumChild.stderr.pipe(seleniumLog);
+
+  process.on('uncaughtException', function (err2) {
+    console.error(err2);
+    console.log(err2.stack);
+    seleniumChild.kill('SIGINT');
+    throw err2;
+  });
+
+  startServer(onServerStarted(seleniumChild));
+}
+
+
+function onSeleniumInstalled(err) {
+  if (err) {
+    console.error(err);
+    console.log(err.stack);
+    throw err;
+  }
+
+  selenium.start({seleniumArgs: ['-debug']}, onSeleniumStarted);
+}
+
+
+selenium.install({}, onSeleniumInstalled);
