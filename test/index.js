18d07513911140457cee043582f2ff289d3fd2ca
Nik Butenko <nik@butenko.me>
2015-12-02 00:02:19 +1100

    Minimal test app

---
+++ test/index.js
@@ -0,0 +1,11 @@
+var app = document.createElement('div');
+
+app.id = 'app';
+document.body.appendChild(app);
+
+
+var header = document.createElement('h1');
+header.innerHTML = 'Hello, World!';
+
+
+app.appendChild(header);
